2de88218b3ae1ddd671980c4f95886ae12d7533a
oussemaRidene <44279853+oussemaRidene@users.noreply.github.com>
2019-01-14 14:58:39 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+// Initialize collapse button
+$(".button-collapse").sideBar();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//$('.collapsible').collapsible();
